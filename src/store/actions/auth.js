16a6092eed07ce7ea5ffd2a99f4a7d610107d2a2
Andy Lam <>
2019-06-07 13:23:54 +0800

    Update new group creation

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import firebase from "react-native-firebase";
-import { USER_DETAILS, SIGN_OUT } from "../constants";
+import { USER_DETAILS } from "../constants";
+import { createGroup } from "./groups";
 
 // action creator for authDetails for the phone authentication
 // response from sign in after code is sent
@@ -9,16 +10,6 @@ export const setUserDetails = userDetails => {
     payload: userDetails
   };
 };
-
-// export const signOutUser = dispatch => {
-//   dispatch({ type: SIGN_OUT });
-// }
-
-// export const signOutUser = () => {
-//   return {
-//     type: SIGN_OUT,
-//   }
-// }
 
 export const createAccount = (username, pictureUri) => async dispatch => {
   //upload picture to firebase storage
@@ -47,35 +38,14 @@ export const createAccount = (username, pictureUri) => async dispatch => {
 };
 
 const userDetailsToDatabase = user => async dispatch => {
-  const savedMessagesGroup = {
-    last_message: {
-      data: "",
-      image: "",
-      timestamp: "",
-      user_id: "",
-      video: ""
-    },
-    users_info: {
-      user_id: user._user.uid
-    }
-  };
-
+  const uid = user._user.uid;
   await firebase
     .database()
-    .ref(`groups`)
-    .push(savedMessagesGroup)
-    .then(async res => {
-      const groups = {};
-      groups[res.path] = true;
-      const user2 = { ...user._user, groups };
-      await firebase
-        .database()
-        .ref(`users/${user._user.uid}`)
-        .set(user2);
-      await firebase
-        .database()
-        .ref(`phoneNumbers/${user._user.phoneNumber}`)
-        .set(user2);
-    })
-    .catch(err => console.log(err));
+    .ref(`users/${uid}`)
+    .set(user2);
+  await firebase
+    .database()
+    .ref(`phoneNumbers/${user._user.phoneNumber}`)
+    .set(user2);
+  createGroup("Saved Messages", uid, "This is your saved messages!");
 };
--- src/store/actions/groups.js
+++ src/store/actions/groups.js
@@ -1,32 +1,62 @@
 import firebase from "react-native-firebase";
+import uuidv4 from "uuid/v4";
+const db = firebase.database();
 
-export const createGroup = (groupName, users) => async dispatch => {
-  const users_info = {};
-  users.map(user => (users_info[user.uid] = true));
+export const addGroupToUser = (groupID, uid) => async () => {
+  await db
+    .ref(`users/${uid}/groups`)
+    .child(groupID)
+    .set(true)
+    .catch(err => console.log(err));
+};
+
+export const newGroupCreator = (
+  groupName,
+  groupID,
+  users,
+  data
+) => async () => {
   const newGroup = {
     groupName,
     last_message: {
-      data: "",
+      data,
       image: "",
-      timestamp: "",
+      timestamp: new Date().getTime(),
       user_id: "",
       video: ""
     },
-    users_info
+    users
   };
 
   await firebase
     .database()
     .ref(`groups`)
-    .push(newGroup)
-    .then(res => {
-      const path = res.path;
-      for (let user in users) {
-        firebase
-          .database()
-          .ref(`users/${user.uid}`)
-          .child("groups")
-          .set({ [path]: true });
-      }
-    });
+    .child(groupID)
+    .set(newGroup)
+    .catch(err => console.log(err));
+};
+
+export const createGroup = (
+  groupName,
+  user,
+  data,
+  users = []
+) => async dispatch => {
+  let users_info = { [user]: true };
+  const groupID = uuidv4();
+
+  addGroupToUser(groupID, user);
+
+  for (let user of users) {
+    await db
+      .ref("phoneNumbers")
+      .child(`${user.phoneNumbers[0].number.replace(/\s/g, "")}`)
+      .once("value", async data => {
+        uid = data.child("uid").val();
+        users_info = { ...users_info, [uid]: true };
+        await dispatch(addGroupToUser(groupID, uid));
+      });
+  }
+
+  await dispatch(newGroupCreator(groupName, groupID, users_info, data));
 };
