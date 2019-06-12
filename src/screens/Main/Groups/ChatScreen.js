a4e370a343df435882af69676110013cf0558ad0
Andy Lam <>
2019-06-12 16:55:24 +0800

    Last message on groupscreen

--- src/screens/Main/Groups/ChatScreen.js
+++ src/screens/Main/Groups/ChatScreen.js
@@ -1,37 +1,58 @@
 import React, { Component } from "react";
-import { SafeAreaView, Text, View, TextInput, Dimensions, StyleSheet, KeyboardAvoidingView, Keyboard, Platform } from "react-native";
-import { TouchableOpacity, FlatList, TouchableWithoutFeedback } from "react-native-gesture-handler";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform
+} from "react-native";
+import {
+  TouchableOpacity,
+  FlatList,
+  TouchableWithoutFeedback
+} from "react-native-gesture-handler";
 import firebase from "react-native-firebase";
 import { connect } from "react-redux";
-import { fetchedConversation } from "../../../store/actions/messages"
+import {
+  fetchedConversation,
+  updateLastMessage
+} from "../../../store/actions/messages";
 import MyIcon from "../../../components/MyIcon";
 import _ from "lodash";
 
 class ChatScreen extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          uid: this.props.uid,
-          groupID: this.props.navigation.getParam("group").groupID,
-          textMessage: '',
-          prevDay: (new Date()).getDay(),
-      }
-      this.convertTime = this.convertTime.bind(this);
-      this.sendMessage = this.sendMessage.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.convertDate = this.convertDate.bind(this);
+    super(props);
+    this.state = {
+      uid: this.props.uid,
+      groupID: this.props.navigation.getParam("group").groupID,
+      textMessage: "",
+      prevDay: new Date().getDay()
+    };
+    this.convertTime = this.convertTime.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.convertDate = this.convertDate.bind(this);
   }
-  
-  messagesRef = firebase.database().ref('messages');
+
+  messagesRef = firebase.database().ref("messages");
 
   static navigationOptions = ({ navigation }) => {
     return {
       headerTintColor: "#fff",
       headerTitle: navigation.getParam("group").groupName,
       headerRight: (
-        <TouchableOpacity onPress={() => navigation.navigate("CreateEvents", {
-          groupID: navigation.getParam("group").groupID
-        })}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("CreateEvents", {
+              groupID: navigation.getParam("group").groupID
+            })
+          }
+        >
           <MyIcon
             name="ios-add"
             size={32}
@@ -40,22 +61,27 @@ class ChatScreen extends Component {
           />
         </TouchableOpacity>
       )
-    }
+    };
   };
 
   componentWillMount() {
     const groupID = this.state.groupID;
-    this.messagesRef.child(`${groupID}`).on('value', snapshot => {
-      this.props.dispatch(fetchedConversation(groupID, _.sortBy(_.values(snapshot.val()), 'timestamp')));
-    })
+    this.messagesRef.child(`${groupID}`).on("value", snapshot => {
+      this.props.dispatch(
+        fetchedConversation(
+          groupID,
+          _.sortBy(_.values(snapshot.val()), "timestamp")
+        )
+      );
+    });
     this.keyboardDidShowListener = Keyboard.addListener(
-      'keyboardDidShow',
+      "keyboardDidShow",
       this.scrollToBottom
-    )
+    );
     this.keyboardDidHideListener = Keyboard.addListener(
-      'keyboardDidHide',
+      "keyboardDidHide",
       this.scrollToBottom
-    )
+    );
   }
 
   componentWillUnmount() {
@@ -64,8 +90,8 @@ class ChatScreen extends Component {
   }
 
   scrollToBottom = (contentHeight, contentWidth) => {
-    this.refs.messageList.scrollToEnd({animated: true})
-  }
+    this.refs.messageList.scrollToEnd({ animated: true });
+  };
 
   handleChange = key => val => {
     this.setState({
@@ -74,49 +100,49 @@ class ChatScreen extends Component {
   };
 
   convertTime = time => {
-      let d = new Date(time);
-      let c = new Date();
-      let result = (d.getHours() < 10 ? 0 : '') + d.getHours() + ":";
-      result += (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
-      return result;
-  }
+    let d = new Date(time);
+    let c = new Date();
+    let result = (d.getHours() < 10 ? 0 : "") + d.getHours() + ":";
+    result += (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
+    return result;
+  };
 
   convertDate = dateObj => {
     dateObj = new Date(dateObj);
     const date = dateObj.getDate();
     let month = dateObj.getMonth();
     let hour = dateObj.getHours();
-    hour = hour < 10 ? '0' + hour : hour;
+    hour = hour < 10 ? "0" + hour : hour;
     let minute = dateObj.getMinutes();
-    minute = minute < 10 ? '0' + minute : minute;
+    minute = minute < 10 ? "0" + minute : minute;
     let day = dateObj.getDay();
-    switch(day) {
+    switch (day) {
       case 0:
-        day = "Sunday"
+        day = "Sunday";
         break;
       case 1:
-        day = "Monday"
+        day = "Monday";
         break;
       case 2:
-        day = "Tuesday"
+        day = "Tuesday";
         break;
       case 3:
-        day = "Wednesday"
+        day = "Wednesday";
         break;
       case 4:
-        day = "Thursday"
+        day = "Thursday";
         break;
       case 5:
-        day = "Friday"
+        day = "Friday";
         break;
       case 6:
-        day = "Saturday"
+        day = "Saturday";
         break;
       default:
         day = "No day defined";
         break;
     }
-    switch(month) {
+    switch (month) {
       case 0:
         month = "Jan";
         break;
@@ -158,7 +184,7 @@ class ChatScreen extends Component {
         break;
     }
     return `${day}, ${date} ${month}, ${hour}:${minute}`;
-  }
+  };
 
   // sameDay = (dateOfMessage, message) => {
   //   console.log("props date = " + this.props.prevDate);
@@ -172,103 +198,145 @@ class ChatScreen extends Component {
   // }
 
   sendMessage = () => {
-      const groupID = this.state.groupID;
-      if(this.state.textMessage.length > 0) {
-        const msgID = this.messagesRef.child(`${groupID}`).push().key;
-        let message = {
-            messageType: "text",
-            message: this.state.textMessage,
-            timestamp: firebase.database.ServerValue.TIMESTAMP,
-            sender: this.state.uid
-        }
-        this.messagesRef.child(`${groupID}`).child(`${msgID}`).set(message);
-        this.setState({ textMessage: ''})
-      }
+    const groupID = this.state.groupID;
+    if (this.state.textMessage.length > 0) {
+      const msgID = this.messagesRef.child(`${groupID}`).push().key;
+      console.log(this.state);
+      let message = {
+        messageType: "text",
+        message: this.state.textMessage,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        sender: this.state.uid,
+        username: this.props.username
+      };
+      this.messagesRef
+        .child(`${groupID}`)
+        .child(`${msgID}`)
+        .set(message);
+      updateLastMessage(groupID, message);
+      this.setState({ textMessage: "" });
+    }
   };
 
   renderRow = ({ item }) => {
-    if(item.messageType === "text"){
-      return(
-        <View style={item.sender === this.props.uid ? styles.myMessageBubble : styles.yourMessageBubble}>
-          <View style={{flexWrap: "wrap"}}>
-            <Text style={{color: '#fff', padding: 7, fontSize: 16}}>
+    if (item.messageType === "text") {
+      return (
+        <View
+          style={
+            item.sender === this.props.uid
+              ? styles.myMessageBubble
+              : styles.yourMessageBubble
+          }
+        >
+          <View style={{ flexWrap: "wrap" }}>
+            <Text style={{ color: "#fff", padding: 7, fontSize: 16 }}>
               {item.message}
             </Text>
           </View>
-          <View style={{justifyContent: 'flex-end'}}>
-            <Text style={{color:'#eee', paddingRight: 13, paddingBottom: 7, fontSize: 10}}>
+          <View style={{ justifyContent: "flex-end" }}>
+            <Text
+              style={{
+                color: "#eee",
+                paddingRight: 13,
+                paddingBottom: 7,
+                fontSize: 10
+              }}
+            >
               {this.convertTime(item.timestamp)}
             </Text>
           </View>
         </View>
-      )
-    } else if(item.messageType === "event") {
+      );
+    } else if (item.messageType === "event") {
       return (
-        <View style={item.sender === this.props.uid ? styles.myEventBubble : styles.yourEventBubble}>
+        <View
+          style={
+            item.sender === this.props.uid
+              ? styles.myEventBubble
+              : styles.yourEventBubble
+          }
+        >
           <View style={styles.eventBubbleContent}>
             <View>
-              <Text style={{...styles.eventDetails, fontWeight: "bold"}}>
+              <Text style={{ ...styles.eventDetails, fontWeight: "bold" }}>
                 {item.event.title}
               </Text>
               <Text style={styles.eventDetails}>
-                {this.convertDate(item.event.from) + " to\n" + this.convertDate(item.event.to)}
+                {this.convertDate(item.event.from) +
+                  " to\n" +
+                  this.convertDate(item.event.to)}
               </Text>
-              <Text style={{...styles.eventDetails, display: item.event.location ? 'flex' : 'none'}}>
+              <Text
+                style={{
+                  ...styles.eventDetails,
+                  display: item.event.location ? "flex" : "none"
+                }}
+              >
                 {item.event.location}
               </Text>
             </View>
-            <View style={{justifyContent: 'flex-end'}}>
-              <Text style={{color:'#eee', paddingRight: 13, paddingBottom: 7, fontSize: 10}}>
+            <View style={{ justifyContent: "flex-end" }}>
+              <Text
+                style={{
+                  color: "#eee",
+                  paddingRight: 13,
+                  paddingBottom: 7,
+                  fontSize: 10
+                }}
+              >
                 {this.convertTime(item.timestamp)}
               </Text>
             </View>
           </View>
           <View style={styles.eventBubbleButtons}>
-            <View style={{flex: 1}}>
+            <View style={{ flex: 1 }}>
               <TouchableOpacity style={styles.acceptButton}>
                 <Text style={styles.invitationButton}>Accept</Text>
               </TouchableOpacity>
             </View>
-            <View style={{flex: 1}}>
+            <View style={{ flex: 1 }}>
               <TouchableOpacity style={styles.rejectButton}>
                 <Text style={styles.invitationButton}>Reject</Text>
               </TouchableOpacity>
             </View>
           </View>
         </View>
-      )
+      );
     }
-  }
-
+  };
 
   render() {
-    let { height } = Dimensions.get('window');
+    let { height } = Dimensions.get("window");
     return (
-      <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={Platform.OS === 'ios' ? 85 : 0} style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        keyboardVerticalOffset={Platform.OS === "ios" ? 85 : 0}
+        style={{ flex: 1 }}
+      >
         <SafeAreaView>
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.inner}>
-            <FlatList
-              ref="messageList"
-              onContentSizeChange={this.scrollToBottom}
-              style={{padding: 10, height: height * 0.8}}
-              data={this.props.conversation.slice()}
-              renderItem={this.renderRow}
-              keyExtractor={(item, index) => index.toString()}
-            />
-            <View style={styles.chatBox}>
-              <TextInput
-                style={styles.chatInput}
-                value={this.state.textMessage}
-                onChangeText={this.handleChange("textMessage")}
-                placeholder="Write a message"
+            <View style={styles.inner}>
+              <FlatList
+                ref="messageList"
+                onContentSizeChange={this.scrollToBottom}
+                style={{ padding: 10, height: height * 0.8 }}
+                data={this.props.conversation.slice()}
+                renderItem={this.renderRow}
+                keyExtractor={(item, index) => index.toString()}
               />
-              <TouchableOpacity onPress={this.sendMessage}>
-                <Text style={styles.sendBtn}>Send</Text>
-              </TouchableOpacity>
+              <View style={styles.chatBox}>
+                <TextInput
+                  style={styles.chatInput}
+                  value={this.state.textMessage}
+                  onChangeText={this.handleChange("textMessage")}
+                  placeholder="Write a message"
+                />
+                <TouchableOpacity onPress={this.sendMessage}>
+                  <Text style={styles.sendBtn}>Send</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flex: 1 }} />
             </View>
-            <View style={{flex: 1}}/>
-          </View>
           </TouchableWithoutFeedback>
         </SafeAreaView>
       </KeyboardAvoidingView>
@@ -279,108 +347,110 @@ class ChatScreen extends Component {
 const mapStateToProps = (state, ownProps) => {
   const { groupID } = ownProps.navigation.getParam("group");
   const stateOfGroup = state.messagesReducer[groupID] || {};
+  const username = state.authReducer.user.displayName;
   return {
     uid: state.authReducer.user.uid,
     conversation: stateOfGroup.messages || [],
-  }
-}
+    username
+  };
+};
 
 export default connect(mapStateToProps)(ChatScreen);
 
 const styles = StyleSheet.create({
-  inner:{
-    justifyContent: "flex-end",
+  inner: {
+    justifyContent: "flex-end"
   },
-  chatBox:{
-    flexDirection: "row", 
-    alignItems: "center" ,
+  chatBox: {
+    flexDirection: "row",
+    alignItems: "center"
   },
   chatInput: {
     borderWidth: 1,
     borderRadius: 7,
-    width: '80%',
+    width: "80%",
     padding: 10,
     margin: 8,
-    color: 'black',
-    borderColor: 'black'
+    color: "black",
+    borderColor: "black"
   },
   sendBtn: {
-    color: '#1d73d6',
-    fontSize: 20,
+    color: "#1d73d6",
+    fontSize: 20
   },
   yourMessageBubble: {
-    flexDirection:"row",
-    justifyContent: 'space-between',
-    width:"auto",
-    alignSelf: 'flex-start',
-    backgroundColor: '#134782',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "auto",
+    alignSelf: "flex-start",
+    backgroundColor: "#134782",
     borderRadius: 20,
     marginBottom: 8,
     paddingLeft: 5,
-    marginRight: 40,
+    marginRight: 40
   },
   myMessageBubble: {
-    flexDirection:"row",
-    justifyContent: 'space-between',
-    width:"auto",
-    alignSelf: 'flex-end',
-    backgroundColor: '#3a8cbc',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "auto",
+    alignSelf: "flex-end",
+    backgroundColor: "#3a8cbc",
     borderRadius: 20,
     marginBottom: 8,
     paddingLeft: 5,
-    marginLeft: 40,
+    marginLeft: 40
   },
   yourEventBubble: {
-    alignSelf: 'flex-start', 
-    borderRadius: 20, 
-    marginBottom: 8, 
-    backgroundColor:"#134782", 
-    width:"auto",
+    alignSelf: "flex-start",
+    borderRadius: 20,
+    marginBottom: 8,
+    backgroundColor: "#134782",
+    width: "auto",
     marginRight: 40
   },
   myEventBubble: {
-    alignSelf: 'flex-end', 
-    borderRadius: 20, 
-    marginBottom: 8, 
-    backgroundColor:"#3a8cbc", 
-    width:"auto",
+    alignSelf: "flex-end",
+    borderRadius: 20,
+    marginBottom: 8,
+    backgroundColor: "#3a8cbc",
+    width: "auto",
     marginLeft: 50
   },
   invitationButton: {
-    textAlign: "center", 
-    color: "#fff", 
+    textAlign: "center",
+    color: "#fff",
     fontSize: 15,
     fontWeight: "bold"
   },
   eventDetails: {
-    color: '#fff', 
-    padding: 7, 
-    fontSize: 16, 
-    textDecorationLine: 'underline', 
+    color: "#fff",
+    padding: 7,
+    fontSize: 16,
+    textDecorationLine: "underline",
     flex: 1
   },
-  eventBubbleContent:{
-    flexWrap: 'nowrap',
+  eventBubbleContent: {
+    flexWrap: "nowrap",
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingLeft: 5,
+    paddingLeft: 5
   },
   eventBubbleButtons: {
-    height: 40, 
-    flexDirection: "row", 
-    justifyContent: "center", 
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center"
   },
   acceptButton: {
-    backgroundColor:"#2cb768",
+    backgroundColor: "#2cb768",
     height: 40,
     justifyContent: "center",
     borderBottomLeftRadius: 20
   },
   rejectButton: {
-    backgroundColor:"#c13f3f",
+    backgroundColor: "#c13f3f",
     height: 40,
     justifyContent: "center",
     borderBottomRightRadius: 20
-  },
-})
+  }
+});
--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,24 +1,35 @@
+import firebase from "react-native-firebase";
+
 import { FETCH_CONVERSATION, CHANGE_PREV_DATE } from "../constants";
 
 export const fetchedConversation = (groupID, messages) => {
-    return {
-        type: FETCH_CONVERSATION,
-        payload: {
-            groupID,
-            messages,
-        }
+  return {
+    type: FETCH_CONVERSATION,
+    payload: {
+      groupID,
+      messages
     }
-}
+  };
+};
 
 export const changePrevDate = (groupID, prevDate) => {
-    return {
-        type: CHANGE_PREV_DATE,
-        payload: {
-            groupID,
-            prevDate
-        }
+  return {
+    type: CHANGE_PREV_DATE,
+    payload: {
+      groupID,
+      prevDate
     }
-}
+  };
+};
+
+export const updateLastMessage = (groupId, message) => async dispatch => {
+  await firebase
+    .database()
+    .ref(`groups/${groupId}/last_message`)
+    .set(message)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+};
 
 // export const fetchNewMessage = (groupID, message) => {
 //     return {
