728605857fc5fd247e3b77be1808bb80a894082b
Andy Lam <>
2019-06-15 11:46:30 +0800

    Agenda

--- src/screens/Main/Groups/MyCalendar.js
+++ src/screens/Main/Groups/MyCalendar.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Calendar, Agenda } from "react-native-calendars";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Agenda } from "react-native-calendars";
 import ContinueButton from "../.././../components/ContinueButton";
 
 class MyCalendar extends React.Component {
+  state = { events: {} };
+
   renderButton = () => {
     return (
       <TouchableOpacity
@@ -23,11 +25,99 @@ class MyCalendar extends React.Component {
   render() {
     return (
       <View style={{ display: "flex", height: "100%" }}>
-        <Agenda />
+        <Agenda
+          items={this.state.events}
+          loadItemsForMonth={this.loadItems.bind(this)}
+          renderItem={this.renderItem.bind(this)}
+          renderEmptyDate={this.renderEmptyDate.bind(this)}
+          rowHasChanged={this.rowHasChanged.bind(this)}
+          // markingType={'period'}
+          // markedDates={{
+          //    '2017-05-08': {textColor: '#666'},
+          //    '2017-05-09': {textColor: '#666'},
+          //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+          //    '2017-05-21': {startingDay: true, color: 'blue'},
+          //    '2017-05-22': {endingDay: true, color: 'gray'},
+          //    '2017-05-24': {startingDay: true, color: 'gray'},
+          //    '2017-05-25': {color: 'gray'},
+          //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+          // monthFormat={'yyyy'}
+          // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+          //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+        />
         {this.renderButton()}
       </View>
     );
   }
+
+  loadItems(day) {
+    // setTimeout(() => {
+    //   for (let i = -15; i < 85; i++) {
+    //     const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+    //     const strTime = this.timeToString(time);
+    //     if (!this.state.events[strTime]) {
+    //       this.state.events[strTime] = [];
+    //       const numItems = Math.floor(Math.random() * 5);
+    //       for (let j = 0; j < numItems; j++) {
+    //         this.state.events[strTime].push({
+    //           name: "Item for " + strTime,
+    //           height: Math.max(50, Math.floor(Math.random() * 150))
+    //         });
+    //       }
+    //     }
+    //   }
+    //   //console.log(this.state.events);
+    //   const newItems = {};
+    //   Object.keys(this.state.events).forEach(key => {
+    //     newItems[key] = this.state.events[key];
+    //   });
+    //   this.setState({
+    //     items: newItems
+    //   });
+    // }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, { height: item.height }]}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
+  }
+});
 
 export default MyCalendar;
