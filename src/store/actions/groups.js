e2e6a857a79738fc208d2975fda642f87d4f204b
Andy Lam <>
2019-06-24 17:18:43 +0800

    fix group thinghjg

--- src/store/actions/groups.js
+++ src/store/actions/groups.js
@@ -109,19 +109,17 @@ export const createGroup = (
 ) => async dispatch => {
   let users_info = { [myuser]: true };
   const groupID = uuidv4();
-  addGroupToUser(groupID, myuser);
+  for (let user of users) {
+    db.ref("phoneNumbers")
+      .child(`${user.phoneNumbers[0].number.replace(/\s/g, "")}`)
+      .once("value", data => {
+        uid = data.child("uid").val();
+        users_info = { ...users_info, [uid]: true };
+      });
+  }
 
   const url = await addGroupPicture(groupPicture);
   newGroupCreator(groupName, groupID, url, users_info, data).then(() => {
-    for (let user of users) {
-      db.ref("phoneNumbers")
-        .child(`${user.phoneNumbers[0].number.replace(/\s/g, "")}`)
-        .once("value", data => {
-          uid = data.child("uid").val();
-          users_info = { ...users_info, [uid]: true };
-          addGroupToUser(groupID, uid);
-        });
-    }
     return db
       .ref("groups")
       .child(`${groupID}`)
@@ -130,8 +128,20 @@ export const createGroup = (
         const newGroup = snapshot.val();
         dispatch(addNewGroup(groupID, newGroup));
         dispatch(sortGroups());
-        return true;
       })
-      .catch(e => console.log(e));
+      .catch(e => console.log(e))
+      .then(() => {
+        for (let user of users) {
+          db.ref("phoneNumbers")
+            .child(`${user.phoneNumbers[0].number.replace(/\s/g, "")}`)
+            .once("value", data => {
+              addGroupToUser(groupID, uid);
+            });
+        }
+      })
+      .then(() => {
+        addGroupToUser(groupID, myuser);
+        return true;
+      });
   });
 };
