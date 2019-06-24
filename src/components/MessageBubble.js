9bd357041d10c8594b214b1afec42c5cf6bb6c0d
Andy Lam <>
2019-06-24 15:37:27 +0800

    Timestamp fix

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -5,32 +5,46 @@ import Text from "./Text";
 class MessageBubble extends React.Component {
   render() {
     return (
-      <View style={this.props.style}>
-        {this.props.item.sender !== this.props.uid && (
-          <View style={{ padding: 2 }}>
-            <Text white header semibold>
-              {this.props.item.username}
-            </Text>
-          </View>
-        )}
-        <View style={{ flexDirection: "row" }}>
-          <View style={{ flexWrap: "wrap" }}>
-            <Text header white style={{ padding: 7 }}>
+      <View style={[this.props.style, { flexDirection: "row" }]}>
+        <View>
+          {this.props.item.sender !== this.props.uid && (
+            <View
+              style={{
+                padding: 2,
+                flexDirection: "row",
+                justifyContent: "space-between"
+              }}
+            >
+              <Text white header semibold>
+                {this.props.item.username}
+              </Text>
+            </View>
+          )}
+          <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+            <Text
+              header
+              white
+              style={{
+                padding: 7,
+                width: "auto",
+                maxWidth: this.props.maxWidth * 0.85
+              }}
+            >
               {this.props.item.message}
             </Text>
           </View>
-          <View style={{ justifyContent: "flex-end" }}>
-            <Text
-              style={{
-                color: "#eee",
-                paddingRight: 13,
-                paddingBottom: 7,
-                fontSize: 10
-              }}
-            >
-              {this.props.convertTime(this.props.item.timestamp)}
-            </Text>
-          </View>
+        </View>
+        <View style={{ alignItems: "flex-end", justifyContent: "flex-end" }}>
+          <Text
+            style={{
+              color: "#eee",
+              paddingBottom: 7,
+              paddingRight: 10,
+              fontSize: 10
+            }}
+          >
+            {this.props.convertTime(this.props.item.timestamp)}
+          </Text>
         </View>
       </View>
     );
