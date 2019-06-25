54b2c2be9a0e9ef35bcee65155f04029f46e1fa0
Andy Lam <>
2019-06-25 11:32:57 +0800

    Implement image caching

--- src/components/GroupPicture.js
+++ src/components/GroupPicture.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { Image, Dimensions } from "react-native";
+import { View, Image, Dimensions } from "react-native";
+import imageCacheHoc from "react-native-image-cache-hoc";
+import defaultPicture from "../assets/default_profile.png";
 
 class GroupPicture extends React.Component {
+  cacheImage = (source, style) => {
+    const CacheableImage = imageCacheHoc(Image, {
+      defaultPlaceholder: {
+        component: Image,
+        props: {
+          style: style,
+          source: defaultPicture
+        }
+      }
+      // validProtocols: ["http", "https"]
+      // fileHostWhitelist: ["localhost", "firebasestorage"]
+    });
+    return <CacheableImage source={source} style={style} />;
+  };
+
   render() {
     const { value } = this.props;
+    let src = this.props.source;
+    if (!this.props.source.uri) {
+      src = {
+        uri: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+      };
+    }
     return (
-      <Image
-        source={this.props.source}
-        style={{
+      <View>
+        {this.cacheImage(src, {
           height: Dimensions.get("window").width * value,
           width: Dimensions.get("window").width * value,
           borderRadius: (Dimensions.get("window").width * value) / 2
-        }}
-      />
+        })}
+      </View>
     );
   }
 }
