ea8b3c992aff48922d8657b83cd6cbcc0394a49f
Andy Lam <>
2019-06-26 23:18:42 +0800

    User Details aesthetics

--- src/screens/Auth/UserDetails.js
+++ src/screens/Auth/UserDetails.js
@@ -1,54 +1,33 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Dimensions,
-  TouchableHighlight
-} from "react-native";
+import { View, StyleSheet, TextInput, Dimensions } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import ImagePicker from "../../components/ImagePickerComponent";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 
-import MyIcon from "../../components/MyIcon";
+import Input from "../../components/Input";
+import Text from "../../components/Text";
+import Button from "../../components/Button";
 import { createAccount } from "../../store/actions/auth";
 import defaultPicture from "../../assets/default_profile.png";
-import AsyncStorage from "@react-native-community/async-storage";
 import Spinner from "../../components/Spinner";
 
 class UserDetails extends React.Component {
   state = { loading: false };
-  storeData = async (key, val) => {
-    try {
-      val = JSON.stringify(val);
-      if (val) {
-        await AsyncStorage.setItem(key, val);
-      } else {
-        console.log("no value");
-      }
-    } catch (e) {
-      // saving error
-      console.log(e);
-    }
-  };
 
   handleSubmit = async values => {
     this.setState({ loading: true });
-    this.storeData("profilePicture", values.profilepicture.uri);
-    await this.props.createAccount(
-      values.username,
-      values.profilepicture.uri,
-      values.profilepicture.fileName.split(".")[1]
-    );
-    this.props.navigation.navigate("App");
+    this.props
+      .createAccount(
+        values.username,
+        values.profilepicture.uri,
+        values.profilepicture.fileName.split(".")[1]
+      )
+      .then(() => this.props.navigation.navigate("App"));
   };
 
   renderInput = ({ input, label, meta }) => {
-    return (
-      <TextInput {...input} style={styles.textInput} placeholder={label} />
-    );
+    return <Input {...input} style={styles.textInput} placeholder={label} />;
   };
 
   renderImagePicker = props => {
@@ -73,19 +52,15 @@ class UserDetails extends React.Component {
           component={this.renderInput}
           label="Enter username"
         />
-        <TouchableHighlight
-          title="Continue"
-          style={styles.button}
-          // weird syntax to get redux-form to bind submit this.handleSubmit() function to the designated form submit
+        <Button
+          shadow
           onPress={this.props.handleSubmit(this.handleSubmit.bind(this))}
+          style={{ width: "50%" }}
         >
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Text style={{ color: "blue", marginRight: 7 }}>Continue</Text>
-            <View style={{ marginTop: 1 }}>
-              <MyIcon name="ios-arrow-forward" size={13} color="blue" />
-            </View>
-          </View>
-        </TouchableHighlight>
+          <Text semibold header center black>
+            Continue
+          </Text>
+        </Button>
         {this.state.loading && <Spinner />}
       </SafeAreaView>
     );
@@ -94,23 +69,20 @@ class UserDetails extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: "25%"
+    bottom: "10%"
   },
   text: {
     fontSize: 16,
     marginBottom: 10
   },
   textInput: {
-    marginTop: "10%",
+    margin: "5%",
     height: 16,
     borderBottomColor: "#bbb",
     borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  button: {
-    marginTop: 20
   }
 });
 
