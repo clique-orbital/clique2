0d6aa85541f829ea8f66257701cf51e23fa9f8fd
Chester Sim <chestersimdy@gmail.com>
2019-07-10 00:56:20 +0800

    Add system message when user responds to invitation

--- src/components/SystemMessageBubble.js
+++ src/components/SystemMessageBubble.js
@@ -4,10 +4,14 @@ import Text from "./Text";
 
 export default class SystemMessageBubble extends Component {
   render() {
+    let backgroundColor = "#2474f7";
+    if (this.props.message.includes("attending")) {
+      backgroundColor = this.props.message.includes("not") ? "#E83838" : "#65c681";
+    }
     return (
-      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-        <View style={{ width: "auto", marginHorizontal: 20, backgroundColor: "#2374f7", paddingHorizontal: 10, paddingVertical: 3, borderRadius: 15 }}>
-          <Text white>{this.props.message}</Text>
+      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginVertical: 5, }}>
+        <View style={{ width: "auto", backgroundColor, paddingHorizontal: 10, paddingVertical: 3, borderRadius: 15 }}>
+          <Text white center>{this.props.message}</Text>
         </View>
       </View>
     )
